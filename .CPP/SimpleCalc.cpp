e1aeab71d5aadee21e5396d3b119a8783d2b2542
JWGStudios <JWGsStudios@gmail.com>
2021-05-30 17:08:23 +0100

    Added the calculator files

---
+++ .CPP/SimpleCalc.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	    int x = 0;
+		int y = 0;
+		int z = 0;
+
+
+	printf("Simple Addition Calculator");
+	cout << "" << endl;
+	printf("Input Number 1:");
+	cout << "" << endl;
+	cin >> x;
+	cout << "" << endl;
+	printf("Input Number 2:");
+    cout << "" << endl;
+	cin >> y;
+	z = x + y;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "Your Answer: " << z;
+
+
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	system("pause");
+
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+
+
+
+
+
+
+
+
+
+
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+
+	printf("Simple Subtraction Calculator");
+	cout << "" << endl;
+	printf("Input Number 1:");
+	cout << "" << endl;
+	cin >> x;
+	cout << "" << endl;
+	printf("Input Number 2:");
+	cout << "" << endl;
+	cin >> y;
+	z = x / y;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "Your Answer: " << z;
+
+
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	system("pause");
+
+
+
+
+
+
+
+
+
+
+	printf("Simple Multiplication Calculator");
+	cout << "" << endl;
+	printf("Input Number 1:");
+	cout << "" << endl;
+	cin >> x;
+	cout << "" << endl;
+	printf("Input Number 2:");
+	cout << "" << endl;
+	cin >> y;
+	z = x * y;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "Your Answer: " << z;
+
+
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	system("pause");
+
+
+
+
+
+
+
+
+
+
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+
+	printf("Simple Division Calculator");
+	cout << "" << endl;
+	printf("Input Number 1:");
+	cout << "" << endl;
+	cin >> x;
+	cout << "" << endl;
+	printf("Input Number 2:");
+	cout << "" << endl;
+	cin >> y;
+	z = x / y;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "Your Answer: " << z;
+
+
+	cout << "" << endl;
+	cout << "" << endl;
+	cout << "" << endl;
+
+
+
+	
+	printf("JWGStudios© Copyright 2021, https://jwgstudios.github.io/legal");
+	cout << "" << endl;
+	cout << "" << endl;
+	system("pause");
+	return 0;
+}
